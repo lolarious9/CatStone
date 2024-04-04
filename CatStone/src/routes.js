d84b70452e2ba18d7e742f51da56de7540530c12
Tim-Waterman <157667406+Tim-Waterman@users.noreply.github.com>
2024-04-04 11:28:04 -0500

    added route for record viewing

--- CatStone/src/routes.js
+++ CatStone/src/routes.js
@@ -1,5 +1,6 @@
 import Home from "./views/HomePage.vue"
 import CreateLoan from "./views/CreateLoan.vue"
+import RecordViewing from "./views/RecordViewing.vue"
 export default{
     "HomePage":{ 
         sfc: Home,
@@ -9,5 +10,9 @@ export default{
    "CreateLoan":{
         sfc:CreateLoan,
         title:"CreateLoan"  
-    }
+    },
+    "RecordViewing":{
+        sfc:RecordViewing,
+        title:"RecordViewing"
+    },
 }
