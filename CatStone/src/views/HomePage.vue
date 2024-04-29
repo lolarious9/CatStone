f19b8c6556446247cfb764cc3b9d4681b1366bd2
Quinn Rafferty <qrafferty@mac.com>
2024-04-29 14:15:15 -0500

    Added a selector for quick viewing account holders.

--- CatStone/src/views/HomePage.vue
+++ CatStone/src/views/HomePage.vue
@@ -2,46 +2,63 @@
   <v-card
     v-if="ready"
   >
-    <v-card-title>Accounts: {{ numAccouts }}</v-card-title>
-    <v-card-text>
-      {{ tmp }}
+    <v-card-title>
+      Accounts: {{ numAccouts }}
+      <v-select
+        v-model="selected"
+        item-title="name"
+        :items="borrowers"
+        return-object
+      />
+    </v-card-title>
+    <v-card-text v-if="selected">
+      <p class="text-h6 font-weight-black">
+        Name: {{ selected.name }}
+      </p>
+      Loans: ${{ selectedAmts.loans }}
+      Payments: ${{ selectedAmts.payments }}
+      Total: ${{ selectedAmts.loans - selectedAmts.payments }}
     </v-card-text>
   </v-card>
 </template>
 
 
 <script setup>
- import {  ref,computed} from 'vue'
+  import {  ref,computed} from 'vue'
+ 
+  const selected  = ref(null);
   
    function getBorrowers (){
     const ready = ref(false)
-    const tmp = ref(null);
-
+    const borrowers = ref(null);
     //execute the search
     const exe = async()=>
     {
       ready.value=false;
-      tmp.value =  await window.dbDispatch.getBorrowers();   
+      borrowers.value =  await window.dbDispatch.getBorrowers();   
       ready.value = true;
     }
 
     exe();
     return {
-      tmp,
+      borrowers,
       ready
     }
   }
   
   
-  let {tmp,ready}= getBorrowers();
+  let {borrowers,ready}= getBorrowers();
 
 
 
  
-  // Computed will change with the underlying object (tmpDataIn for now)
-  //let sumAccts = computed(()=> tmp != null ? tmp.reduce((prev,curr)=> prev+ curr.loanSum,0):"woah");
-  let numAccouts = computed(()=>tmp.value.length != null ?  tmp.value.length : 0)
-
+  // Computed will change with the underlying object (borrowersDataIn for now)
+  //let sumAccts = computed(()=> borrowers != null ? borrowers.reduce((prev,curr)=> prev+ curr.loanSum,0):"woah");
+  let numAccouts = computed(()=>borrowers.value.length != null ?  borrowers.value.length : 0)
+  let selectedAmts = computed(()=>selected.value != null ?  {
+    loans:selected.value.loans.reduce((prev,curr)=> prev+ curr.loanAmount,0),
+    payments:selected.value.payments.reduce((prev,curr)=> prev+ curr.paymentAmount,0)
+  }:"woah");
   
 
 
