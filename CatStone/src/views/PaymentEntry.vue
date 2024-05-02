2b650dda8957cdcd8044518297e0e1c13b06cb9d
Quinn Rafferty <qrafferty@mac.com>
2024-05-02 10:57:45 -0500

    Looks fine to me

--- CatStone/src/views/PaymentEntry.vue
+++ CatStone/src/views/PaymentEntry.vue
@@ -3,11 +3,20 @@
     class="mx-auto"
     width="300"
   >
-    <v-form @submit.prevent>
+    <v-form @submit.prevent="submitPayment">
       <v-text-field
         v-model="Name"
         :rules="nameRules"
         label="Name"
+      />
+      <v-select
+        v-model="selected"
+        :loading="standby"
+        item-title="fullName"
+        :items="namesAndLoans"
+        placeholder="Borrowers"
+        no-data-text="Unable to contact database"
+        return-object
       />
       <v-text-field
         v-model="PaymentAmount"
@@ -26,11 +35,27 @@
 </template>
   
   <script setup>
-    import { ref } from 'vue'
-  
+   import {  ref,computed,} from 'vue'
+  const props = defineProps({borrowers:{type:Array,default(){return []}},ready:Boolean,standby:Boolean})
+  const selected  = ref(null);
+  const success = ref(null);
+  const namesAndLoans = computed(()=>props.borrowers != null ? props.borrowers.map((borrower)=>{
+    return{
+      fullName: `${borrower.first_name} ${borrower.last_name}`,
+      ...borrower
+    }
+  }) : {})
+
     const Name = ref('')
     const PaymentAmount = ref('')
-  
+    const submitPayment = ()=> window.dbDispatch.addPayment({
+      borrowerID: selected.value.borrowerId,
+      paymentAmount: parseFloat(PaymentAmount.value),
+      paymentDate: Date.now()
+    }).then(()=>{success.value = true}).catch((e)=>{
+      console.error(e)
+      success.value = false
+    })
     const nameRules = [
       value => {
         if (value) return true
